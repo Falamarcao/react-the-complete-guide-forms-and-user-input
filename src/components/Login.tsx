a16107e8f2dee393455b43a3970ac65da4dac921
falamarcao <falamarcao@gmail.com>
2024-11-22 00:50:00 -0300

    Managing & Getting User Input via State & Generic Handlers

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,61 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+interface FormData {
+  email: string;
+  password: string;
+}
+
 export default function Login() {
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData((formData) => ({
+      ...formData,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={formData?.email}
+            onChange={handleChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={formData?.password}
+            onChange={handleChange}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        {/* type="button" make the button not submit the form. default is type="submit" */}
+        <button type="button" className="button button-flat">
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
     </form>
